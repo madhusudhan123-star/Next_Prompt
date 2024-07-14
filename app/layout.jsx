feb72a95a78cdded5da31168d66bab56c4798541
Madhusudhan <dmadhusudhan98@gmail.com>
2024-07-14 13:43:22 +0530

    addeded

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,14 @@ export const metadata = {
 const RootLayout = ({children}) => {
     return (
         <html lang="en">
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-EXEDCM9SCQ"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+
+                gtag('config', 'G-EXEDCM9SCQ');
+            </script>
             <body>
                 <Provider>
                     <div className='main'>

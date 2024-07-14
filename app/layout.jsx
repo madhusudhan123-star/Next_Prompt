1beb02176fa18a25732ad178c0e8bb3f8eea4428
Madhusudhan <dmadhusudhan98@gmail.com>
2024-07-14 14:16:27 +0530

    add

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import Script from 'next/script';
 
 export const metadata = {
     title: 'Promptopia',
@@ -10,14 +11,21 @@ export const metadata = {
 const RootLayout = ({children}) => {
     return (
         <html lang="en">
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-EXEDCM9SCQ"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
-                gtag('js', new Date());
-
-                gtag('config', 'G-EXEDCM9SCQ');
-            </script>
+            <head>
+                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-EXEDCM9SCQ"/>
+                <Script
+                    id='google-analytics'
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-EXEDCM9SCQ');
+                        `
+                    }}
+                />
+            </head>
             <body>
                 <Provider>
                     <div className='main'>
